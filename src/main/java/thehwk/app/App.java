94937ad7011672826257f56faaa1b7227bebf94a
(the_hwk) Yastrebov Alexey <ayastrabau@gmail.com>
2022-11-15 14:22:03 +0300

    First commit

---
+++ src/main/java/thehwk/app/App.java
@@ -0,0 +1,13 @@
+package thehwk.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
